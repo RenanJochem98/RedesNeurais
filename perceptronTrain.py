ae792c585fcec4b0123cdd4ec622fa6861f76494
Renan <renanjochem98@gmail.com>
2020-01-27 13:05:28 -0300

    Implementando funcao de treino do perceptron

--- perceptronTrain.py
+++ perceptronTrain.py
@@ -2,11 +2,8 @@
 import numpy as np
 entradas = np.array([[0,0], [1,0], [0,1], [1,1]])
 saidas = np.array([0,0,0,1])
-pesos = np.array([0.0, 0.0, 0.0]) # por causa do numpy, eh interessante colocar "0.0" em ez de apenas 0
+pesos = np.array([0.0, 0.0]) # por causa do numpy, eh interessante colocar "0.0" em ez de apenas 0
 taxaAprendizagem = 0.1
-def soma (e, p):
-    # Funcao dot calcula o produto escalar. (Ingles: dot Product)
-    return e.dot(p)
 
 def stepfunction(soma):
     result = 0
@@ -14,5 +11,20 @@ def stepfunction(soma):
         result = 1
     return result
 
+def calculaSaida (registro):
+    s = registro.dot(pesos) # Funcao "dot" calcula o produto escalar. (Ingles: dot Product)
+    return stepfunction(s)
+
 def treinar():
-    return 0
+    erroTotal = 1
+    while (erroTotal != 0):
+        erroTotal = 0
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+                print('Peso atualizado: '+str(pesos[j]))
+        print("Total de erros: "+ str(erroTotal))
+treinar()
